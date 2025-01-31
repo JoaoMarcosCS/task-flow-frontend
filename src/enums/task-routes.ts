03f6c3c1fdef567a0bf5e4466e6aa3438c69b36b
João Marcos <jmcsjoaomarcos@gmail.com>
2025-01-31 08:02:06 -0300

    fix sintaxe of routes

--- src/enums/task-routes.ts
+++ src/enums/task-routes.ts
@@ -2,12 +2,12 @@ const BASE_ROUTE = "/task";
 
 export enum TaskRoutes {
   CREATE_TASK = `${BASE_ROUTE}`,
-  GET_TASKS_BY_USER_ID = `${BASE_ROUTE}/get-tasks/:userId`,
-  GET_TASK_BY_USER_ID = `${BASE_ROUTE}/get-tasks/:userId/:taskId`,
+  GET_TASKS_BY_USER_ID = `${BASE_ROUTE}/get-tasks/`,
+  GET_TASK_BY_USER_ID = `${BASE_ROUTE}/get-tasks/`,
   ASSIGN_RESPONSIBLE = `${BASE_ROUTE}/assign_responsible`,
   DELETE_RESPONSIBLE = `${BASE_ROUTE}/delete_responsible`,
   GET_PRIORITIES = `${BASE_ROUTE}/priorities`,
   GET_STATUS = `${BASE_ROUTE}/status`,
-  UPDATE_TASK = `${BASE_ROUTE}/:taskId`,
-  DELETE_TASK = `${BASE_ROUTE}/:taskId`,
+  UPDATE_TASK = `${BASE_ROUTE}/`,
+  DELETE_TASK = `${BASE_ROUTE}/`,
 }
