345cebb1537824c2e3e5062c45d7bd34ac3795aa
João Marcos <jmcsjoaomarcos@gmail.com>
2025-01-31 08:03:22 -0300

    style: add navigation bar

---
+++ src/components/commom/menus/SideBar.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { CirclePlus, ClipboardList, House, ListChecks } from "lucide-react";
+import { ActiveLink } from "../ActiveLink";
+import { SignOutDialog } from "@/app/(auth)/components/SignOutDialog";
+import { useUserStore } from "@/store/user.store";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { takeInitialLetter } from "@/utils/take-initial-letter";
+
+export const SideBar = () => {
+  const { user } = useUserStore((state) => state);
+  return (
+    <div className="h-screen flex max-sm:hidden fixed flex-col justify-center items-center ">
+      <div className="">
+        <h1 className="text-yellow-400 font-semibold text-3xl fixed top-0 ms-4 left-0 mt-2">
+          TaskFlow
+        </h1>
+      </div>
+      <div className="rounded-full shadow-md py-6 w-14 gap-10 flex flex-col ms-10 bg-slate-100">
+        <ActiveLink directionTooltip="right" href="/user-home" tooltipText="Home">
+          <House />
+        </ActiveLink>
+
+        <ActiveLink
+          directionTooltip="right"
+          href="/boards"
+          tooltipText="Boards"
+        >
+          <ClipboardList />
+        </ActiveLink>
+
+        <ActiveLink
+          directionTooltip="right"
+          href="/boards/new"
+          tooltipText="Criar board"
+        >
+          <CirclePlus />
+        </ActiveLink>
+
+        <ActiveLink
+          directionTooltip="right"
+          href="/tasks"
+          tooltipText="Tarefas"
+        >
+          <ListChecks />
+        </ActiveLink>
+
+        <ActiveLink
+          directionTooltip="right"
+          href="/profile"
+          tooltipText="Perfil"
+        >
+          <Avatar>
+            <AvatarFallback className="rounded-full bg-yellow-400">
+              {takeInitialLetter(user?.name ?? "")}
+            </AvatarFallback>
+          </Avatar>
+        </ActiveLink>
+
+        <SignOutDialog />
+      </div>
+    </div>
+  );
+};
