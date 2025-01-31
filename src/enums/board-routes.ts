6bb3bf2b1d14a3c00282b96d7b4fbd2bf9d2b41e
João Marcos <jmcsjoaomarcos@gmail.com>
2025-01-31 08:01:53 -0300

    fix: sintaxe of routes

--- src/enums/board-routes.ts
+++ src/enums/board-routes.ts
@@ -2,12 +2,12 @@ const BASE_ROUTE = "/board"; // Alterar aqui caso a base da rota mude
 
 export enum BoardRoutes {
   CREATE_BOARD = `${BASE_ROUTE}`,
-  GET_BOARDS_BY_USER_ID = `${BASE_ROUTE}/get-boards-by-user-id/:userId`,
-  GET_BOARD_BY_USER_ID = `${BASE_ROUTE}/get-board-by-user-id/:userId/:boardId`,
-  UPDATE_BOARD = `${BASE_ROUTE}/:boardId`,
-  DELETE_BOARD = `${BASE_ROUTE}/:boardId`,
-  ADD_MEMBER = `${BASE_ROUTE}/:boardId/add-member`,
-  DELETE_MEMBER = `${BASE_ROUTE}/:boardId/delete-member`,
-  UPDATE_MEMBER_ROLE = `${BASE_ROUTE}/:boardId/update-member-role`,
+  GET_BOARDS_BY_USER_ID = `${BASE_ROUTE}/get-boards-by-user-id/`,
+  GET_BOARD_BY_USER_ID = `${BASE_ROUTE}/get-board-by-user-id/`,
+  UPDATE_BOARD = `${BASE_ROUTE}/`,
+  DELETE_BOARD = `${BASE_ROUTE}/`,
+  ADD_MEMBER = `${BASE_ROUTE}/add-member`,
+  DELETE_MEMBER = `${BASE_ROUTE}/delete-member`,
+  UPDATE_MEMBER_ROLE = `${BASE_ROUTE}/update-member-role`,
   GET_ROLES = `${BASE_ROUTE}/roles`,
 }
